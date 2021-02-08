c06dc1192f932b9df519eb9c93f0d677d1650080
Furkan <frkncesur@gmail.com>
2021-02-09 01:40:00 +0300

    Refactor with Extensions

--- CryptoViewer/Controller/ViewController.swift
+++ CryptoViewer/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -21,7 +21,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
     }
-    
+}
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
     func didUpdatePrice(price: String, currency: String) {
         DispatchQueue.main.async {
             self.bitcoinLabel.text = price
@@ -32,7 +35,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func didFailWtihError(error: Error) {
         print(error)
     }
-    
+}
+
+//MARK: - UIPickerView DataSource and Delegate
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -40,7 +47,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
@@ -50,6 +57,4 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         coinManager.getCoinPrice(for: selectedCurrency)
         
     }
-    
 }
-
